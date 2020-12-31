916c19b7defcc8971dc29d56b479753d355ec797
draganjancikin <dragan.jancikin@gmail.com>
2020-12-31 12:13:33 +0100

    Finished chapter Front Controller

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/init.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+$name = $request->get('name', 'World');
 
-$request = Request::createFromGlobals();
-
-$myIp = "127.0.0.1";
-
-if ($myIp === $request->getClientIp()) {
-  // the client is a known one, so give it some more privilege
-  $name = $request->get('name', 'World');
-  
-  $response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
-  
-  $response->send();
-}
+$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+$response->send();
